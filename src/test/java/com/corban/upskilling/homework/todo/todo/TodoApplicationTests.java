8e5fadfa835fd1f83d1637befe35e72f0b88c41a
ezdyveg <Corban.Stevens@McKesson.com>
2020-05-31 16:18:45 -0700

    trying to trigger travis build

--- src/test/java/com/corban/upskilling/homework/todo/todo/TodoApplicationTests.java
+++ src/test/java/com/corban/upskilling/homework/todo/todo/TodoApplicationTests.java
@@ -20,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TodoApplicationTests {
+    //test
 
     @Autowired
     private MockMvc mockMvc;
